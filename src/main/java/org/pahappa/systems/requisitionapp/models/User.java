4d6f4b8ca21aee366f8c983b1f00356b6bcb484e
AriyoX <ahumuzaariyo@gmail.com>
2024-07-08 12:12:03 +0300

    Made changes to User class

--- src/main/java/org/pahappa/systems/requisitionapp/models/User.java
+++ src/main/java/org/pahappa/systems/requisitionapp/models/User.java
@@ -10,14 +10,33 @@ import java.util.Objects;
 @Entity
 @Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private long id;
+
+    @Column(name = "username", nullable = false, unique = true)
     private String username;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "email", unique = true)
     private String email;
+
+    @Column(name = "password", nullable = false)
     private String password;
+
+    @Column(name = "phone_number")
     private String phoneNumber;
+
+    @Column(name = "gender")
     private Gender gender;
+
+    @Column(name = "role")
     private Role role;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
